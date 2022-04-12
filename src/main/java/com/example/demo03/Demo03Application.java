d5ecfb1b4462d2f9bca12010f69509d935cdce14
jhaooGam <jhaooli@163.com>
2022-04-12 23:35:47 +0800

    修改了文件

---
+++ src/main/java/com/example/demo03/Demo03Application.java
@@ -0,0 +1,25 @@
+package com.example.demo03;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * 134151351
+ */
+@SpringBootApplication
+public class Demo03Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Demo03Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Demo03Application.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！!!!!!!！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+    }
+
+}
